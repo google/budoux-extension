d7d9833892e18ec74c1dfac30eb69458509949a1
Koji Ishii <kojii@chromium.org>
2023-08-16 17:15:35 +0900

    Split DocumentApplier to a separate file (#346)

--- src/content.ts
+++ src/content.ts
@@ -14,66 +14,6 @@
  * limitations under the License.
  */
 
-import {loadDefaultJapaneseParser} from 'budoux';
-import {Applier} from './applier';
-
-// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-let logger: (...data: any[]) => void;
-// logger = console.log;
-
-// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-function logDebug(...args: any[]) {
-  if (logger) logger(...args);
-}
-
-const className = 'BudouX';
-
-class DocumentApplier {
-  static fromDocument(document: Document) {
-    // @ts-expect-error Use a dynamic property of the document.
-    let docApplier = document.budouX;
-    logDebug('fromDocument: ', docApplier);
-    if (!docApplier) {
-      docApplier = new DocumentApplier();
-      // @ts-expect-error Use a dynamic property of the document.
-      document.budouX = docApplier;
-      Applier.defineClassAs(document, className);
-    }
-    return docApplier;
-  }
-
-  async applyToDocument() {
-    const parser = loadDefaultJapaneseParser();
-    const applier = new Applier(parser);
-    applier.className = className;
-
-    if ('chrome' in window && 'storage' in chrome) {
-      await new Promise<void>(resolve => {
-        chrome.storage.sync.get(
-          {
-            separator: '\u200B',
-          },
-          items => {
-            applier.separator = items.separator;
-            resolve();
-          }
-        );
-      });
-    }
-
-    await this.waitForDOMContentLoaded(document);
-
-    applier.applyToElement(document.body);
-  }
-
-  async waitForDOMContentLoaded(document: Document) {
-    logDebug('waitForDOMContentLoaded: ', document.readyState);
-    if (document.readyState !== 'loading') return;
-    await new Promise<void>(resolve => {
-      document.addEventListener('DOMContentLoaded', () => resolve());
-    });
-    logDebug('DOMContentLoaded: ', document.readyState);
-  }
-}
+import {DocumentApplier} from './document_applier';
 
 DocumentApplier.fromDocument(document).applyToDocument();
